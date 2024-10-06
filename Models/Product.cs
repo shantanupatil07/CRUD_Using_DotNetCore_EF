b9c612624c19a02702f57c10f060a13e33168447
shantanupatil07 <shantanupatil700@gmail.com>
2024-10-06 23:25:24 +0530

    nn

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,15 +2,14 @@
 {
     public class Product
     {
-       
-        public int ? ProductId { get; set; }
-        
-        public string ?ProductName { get; set; }
 
-        public string ?ProductDescription { get; set; }
+        public int? ProductId { get; set; }
 
-        public float? ProductPrice { get; set; }
+        public string? ProductName { get; set; }
 
+        public string? ProductDescription { get; set; }
+
+           public float? ProductPrice { get; set; }
         public int? productQuantity { get; set; }
 
     }
